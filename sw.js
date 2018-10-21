aabfad6956ffe2f7f3efc936625ab1009c1ad79c
Vincent Humeau <hello@vincent-humeau.com>
2018-10-21 20:21:17 +0100

    Update offline page color

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 
 // Incrementing CACHE_VERSION will kick off the install event and force previously cached
 // resources to be cached again.
-const CACHE_VERSION = 4;
+const CACHE_VERSION = 5;
 let CURRENT_CACHES = {
   offline: "offline-v" + CACHE_VERSION
 };
