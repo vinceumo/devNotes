54afd66bb4d73d1a303f86a6fa7a28bf4382b124
Vincent Humeau <vincent.humeau@gmail.com>
2019-02-18 18:06:46 +0000

    Deploy from Forestry.io

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 
 // Incrementing CACHE_VERSION will kick off the install event and force previously cached
 // resources to be cached again.
-const CACHE_VERSION = 6;
+const CACHE_VERSION = 7;
 let CURRENT_CACHES = {
   offline: "offline-v" + CACHE_VERSION
 };
